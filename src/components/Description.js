914a05fc494ddf1d5881db8f3d54fca377d6b997
webstar0103 <webstar0103@gmail.com>
2023-01-19 11:14:25 +0900

    update description message

--- src/components/Description.js
+++ src/components/Description.js
@@ -2,10 +2,10 @@
 const Description = () => {
   return (
     <div className="description">
-      This is my <span className="italic">First Application</span> using React
+      This is my <span className="italic">First Web Application</span> using React
       JS. <br /> <br />
       Although it is a very tiny app it still describes how to create components
-      in React, an awesome Javascript Library made by Facebook
+      in React, an awesome Javascript Library made by Facebook.
     </div>
   )
 }
